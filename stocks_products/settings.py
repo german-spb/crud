9ce4437c82431b944a212bfb5aa67adb7edac3c2
Герман <108299345+german-spb@users.noreply.github.com>
2023-04-06 20:41:29 +0300

    Update settings.py

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crud_db',
-        'USER': 'postgres',
-        'PASSWORD': 'gkoretskiy27',
+        'NAME': '*****',
+        'USER': '*****',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
